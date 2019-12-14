e45bf18d6dfa521fc1fe76dbe67d049df250dd38
SOVJava <olegonoid@gmail.com>
2019-12-14 21:51:41 +0300

    add console interface little universe

--- src/main/java/program/Program.java
+++ src/main/java/program/Program.java
@@ -10,6 +10,7 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         Galaxy galaxy2 = null;
+        Universe universe = null;
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("Вас приветствует Виртуальный помошник.\n" +
@@ -83,34 +84,35 @@ public class Program {
                                                 System.out.println("Галактики нет. Создайте галактику!");
                                                 break;
                                             } else {
-
                                                 System.out.println("1 - Добавить планету (по умолчанию)\n" +
                                                         "2 - Добавить случайную планету\n" +
                                                         "3 - Добавить планету через конструктор\n");
                                                 int add = sc.nextInt();
-                                                switch (add) {
-                                                    case 1:
-                                                        Planet planet = new Planet();
-                                                        galaxy2.addPlanet(planet);
-                                                        System.out.println("Планета по умолчанию создана");
-                                                        break;
-                                                    case 2:
-                                                        galaxy2.addPlanet(Generator.generatePlanet());
-                                                        System.out.println("Случайная планета добавлена");
-                                                        break;
-                                                    case 3:
-                                                        System.out.println("Имя планеты");
-                                                        String ti = sc.next();
-                                                        System.out.println("Масса планеты");
-                                                        int we = sc.nextInt();
-                                                        System.out.println("Х координата");
-                                                        int xx = sc.nextInt();
-                                                        System.out.println("У координата");
-                                                        int yy = sc.nextInt();
-                                                        System.out.println("Z координата");
-                                                        int zz = sc.nextInt();
-                                                        galaxy2.addPlanet(new Planet(ti,we,xx,yy,zz));
-                                                        break;
+                                                if (add > 0 && add <= 3) {
+                                                    switch (add) {
+                                                        case 1:
+                                                            Planet planet = new Planet();
+                                                            galaxy2.addPlanet(planet);
+                                                            System.out.println("Планета по умолчанию создана");
+                                                            break;
+                                                        case 2:
+                                                            galaxy2.addPlanet(Generator.generatePlanet());
+                                                            System.out.println("Случайная планета добавлена");
+                                                            break;
+                                                        case 3:
+                                                            System.out.println("Имя планеты");
+                                                            String ti = sc.next();
+                                                            System.out.println("Масса планеты");
+                                                            int we = sc.nextInt();
+                                                            System.out.println("Х координата");
+                                                            int xx = sc.nextInt();
+                                                            System.out.println("У координата");
+                                                            int yy = sc.nextInt();
+                                                            System.out.println("Z координата");
+                                                            int zz = sc.nextInt();
+                                                            galaxy2.addPlanet(new Planet(ti, we, xx, yy, zz));
+                                                            break;
+                                                    }
                                                 }
                                             }
                                             break;
@@ -164,10 +166,67 @@ public class Program {
                         label2:
                         {
                             while (true){
-
-
-
-
+                                System.out.println("Панель управления вселенной:\n" +
+                                        "1 - создать вселенную\n" +
+                                        "2 - запуск генерации вселенной\n" +
+                                        "3 - добавить галактику\n" +
+                                        "4 - найти галактику\n" +
+                                        "5 - удалить галактике\n" +
+                                        "6 - список галактик во вселенной\n" +
+                                        "7 - выход из меню вселенной");
+                                int univ = sc.nextInt();
+                                if (univ>0&&univ<=6) {
+                                    switch (univ) {
+                                        case 1:
+                                            Universe universe2 = new Universe();
+                                            System.out.println("Галактика создана");
+                                            universe = universe2;
+                                            break;
+                                        case 2:
+                                            System.out.println("Генерация запущена");
+                                            universe.behavior();
+                                            break;
+                                        case 3:
+                                            System.out.println("1 - добавить галактику(по умолчанию)\n" +
+                                                    "2 - добавить сгенерированую галактику\n" +
+                                                    "3 - добавить галактику через конструктор");
+                                            int addG = sc.nextInt();
+                                            if (addG > 0 && addG <= 3) {
+                                                switch (addG) {
+                                                    case 1:
+                                                        Galaxy galaxy3 = new Galaxy();
+                                                        universe.addGalaxy(galaxy3);
+                                                        System.out.println("Галактика по умолчанию добавлена");
+                                                        break;
+                                                    case 2:
+                                                        universe.addGalaxy(Generator.generateGalaxy());
+                                                        System.out.println("Сгенерированая галактика добавлена");
+                                                        break;
+                                                    case 3:
+                                                        System.out.println("Введите имя создаваемой галактики: ");
+                                                        String titleG = sc.next();
+                                                        Galaxy galaxy4 = new Galaxy(titleG);
+                                                        System.out.println("Галактика создана");
+                                                        break;
+                                                }
+                                            }
+                                            break;
+                                        case 4:
+                                        case 5:
+                                        case 6:
+                                            for (int i = 0; i < universe.getGalaxies().size(); i++) {
+                                                System.out.println(universe.getGalaxies().get(i).getTitle());
+                                                for (int j = 0; j < universe.getGalaxies().get(i).getPlanets().
+                                                        size(); j++) {
+                                                    System.out.println(universe.getGalaxies().get(i).getPlanets().
+                                                            get(j));
+                                                }
+                                            }
+                                            break;
+                                        case 7:
+                                            break label2;
+                                    }
+                                }
                             }
                         }
                 }
